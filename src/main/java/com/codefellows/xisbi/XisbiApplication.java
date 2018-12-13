3708709b539203e357bc3c1d888962f2828f87bc
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-12-12 20:03:50 -0800

    initial commit from Spring init

---
+++ src/main/java/com/codefellows/xisbi/XisbiApplication.java
@@ -0,0 +1,14 @@
+package com.codefellows.xisbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XisbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XisbiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/codefellows/xisbi/XisbiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codefellows.xisbi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XisbiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
